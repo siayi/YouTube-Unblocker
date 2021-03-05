80a6175ddaa20a502303a2658263b129d795a4d2
JayHales <joshhales2004@gmail.com>
2021-03-05 10:24:53 +0000

    Fixed an error when a function would return 'null' instead of null.

--- app.js
+++ app.js
@@ -5,20 +5,27 @@ var app = express();
 
 app.use('/', express.static('static'));
 
-app.get('/video', function(req, res){
-  try {
-    res.writeHead(200, {'Content-Type': 'video/mp4'});
-    ytdl(req.query.q).pipe(res);
-  }
-  catch {
-    res.status(404).end();
-  }
+app.get('/video', function (req, res) {
+
+    console.log(req.query.q);
+
+    if (req.query.q === 'null')
+        return res.status(404).end();    
+   
+    try {
+        res.writeHead(200, { 'Content-Type': 'video/mp4' });
+        ytdl(req.query.q).pipe(res);
+    }
+    catch {
+        res.status(404).end();
+    }
+
 });
 
-app.get('/search', function(req, res) {
-  ytsr(req.query.q, {limit: 20})
-    .then(r => res.send(r)) // Send the search results to client.
-    .catch(e => res.status(437).end()); // If any errors send custom error code to client to handle itself.
+app.get('/search', function (req, res) {
+    ytsr(req.query.q, { limit: 20 })
+        .then(r => res.send(r)) // Send the search results to client.
+        .catch(e => res.status(437).end()); // If any errors send custom error code to client to handle itself.
 });
 
 
